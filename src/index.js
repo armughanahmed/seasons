1cf2dcff7589b0dc1d719878dc5c16d4993065c4
Armughan Ahmed <armughan.ahmed@yahoo.com>
2020-08-10 01:30:41 +0500

    added conditional logics

--- src/index.js
+++ src/index.js
@@ -4,19 +4,25 @@ import ReactDOM, { render } from "react-dom";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: "" };
     navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({ lat: position.coords.latitude });
       },
       (err) => {
-        console.log(err);
+        this.setState({ errorMessage: err.message });
       }
     );
   }
   //   react says we have to define render
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    if (this.state.lat && !this.state.errorMessage) {
+      return <div>latitude: {this.state.lat}</div>;
+    } else if (!this.state.lat && this.state.errorMessage) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    } else {
+      return <div>Loading...</div>;
+    }
   }
 }
 ReactDOM.render(<App />, document.querySelector("#root"));
