cfdb5eed843870b706df22792b4fdc1bc7ef3565
Armughan Ahmed <armughan.ahmed@yahoo.com>
2020-08-09 19:50:56 +0500

    seasons app inititals

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM, { render } from "react-dom";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null };
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+  //   react says we have to define render
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
+  }
+}
+ReactDOM.render(<App />, document.querySelector("#root"));
